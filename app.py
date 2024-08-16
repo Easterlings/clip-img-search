50f3a24b580b1db55b97b119cbadadad5a2e104f
Easterlings <1299171412@qq.com>
2024-08-16 09:58:10 +0800

    feat:add search dir

--- app.py
+++ app.py
@@ -62,7 +62,29 @@ def import_img():
     folder_path = request.form.get('folderPath')
     print(folder_path)
     save_to_vector_database(folder_path)
+    addtoenv(folder_path)
     return redirect(url_for('import_page'))
+
+def addtoenv(folder_path):
+    global IMG_DIR
+    IMG_DIR.append(folder_path)
+    print(IMG_DIR)
+
+    print("setting config...")
+        # 设置参数
+    with open(".env", 'r', encoding="utf-8") as file:
+        script_content = file.readlines()
+
+        # 遍历文件内容,并修改需要更改的参数
+    for i, line in enumerate(script_content):
+        if line.startswith('IMG_DIR='):
+            str = ","
+            script_content[i] = f'IMG_DIR={str.join(IMG_DIR)}'
+            break
+        # 将修改后的内容写回文件
+    with open(".env", 'w', encoding="utf-8") as file:
+        file.writelines(script_content)
+        print("setting config finished")
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=5000,debug=True)
